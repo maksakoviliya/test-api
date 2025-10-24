0a37f8f99849b091d95d95299709ded227f53609
maksak_il <maksak_il@mail.ru>
2025-10-25 01:07:43 +0300

    Clear old holds

---
+++ app/Console/Commands/ClearOldHolds.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Enums\HoldStatus;
+use App\Models\Hold;
+use Illuminate\Console\Command;
+
+class ClearOldHolds extends Command
+{
+    protected $signature = 'app:clear-old-holds';
+
+    protected $description = 'Clear old holds from the database';
+
+    public function handle(): int
+    {
+        Hold::query()
+            ->where('created_at', '<', now()->subMinutes(15))
+            ->where('status', HoldStatus::HELD)
+            ->delete();
+        
+        return parent::SUCCESS;
+    }
+}
